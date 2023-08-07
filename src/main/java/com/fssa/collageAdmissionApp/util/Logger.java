c441d5c843166d212cc50478a89690753f240a8b
vishali-30 <116253142+vishali-30@users.noreply.github.com>
2023-08-07 18:53:17 +0530

    Add files via upload

---
+++ src/main/java/com/fssa/collageAdmissionApp/util/Logger.java
@@ -0,0 +1,9 @@
+package com.fssa.collageAdmissionApp.util;
+
+public class Logger {
+	public static void info(String msg) {
+		System.out.println(msg);
+	}
+	
+
+}
---
+++ src/test/java/com/fssa/collageAdmissionApp/Dao/TestDepartmentDao.java
@@ -0,0 +1,5 @@
+package com.fssa.collageAdmissionApp.Dao;
+
+public class TestDepartmentDao {
+
+}
