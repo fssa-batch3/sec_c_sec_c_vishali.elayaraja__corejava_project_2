24177fda7dff60efdbdb23c6040025d20cbd90e8
vishali-30 <116253142+vishali-30@users.noreply.github.com>
2023-08-30 20:33:02 +0530

    Update ConnectionUtil.java

--- src/main/java/com/fssa/collage/admission/app/util/ConnectionUtil.java
+++ src/main/java/com/fssa/collage/admission/app/util/ConnectionUtil.java
@@ -10,26 +10,25 @@ public class ConnectionUtil {
 		System.out.println("connection created");
 	}
 
-	public static Connection getConnection() {
-		Connection con = null;
+	 public static Connection getConnection() {
+	        Connection con = null;
 
-//	        String url, userName, passWord;
+	        String url, userName, passWord;
 
-//	            url = System.getenv("DATABASE_HOST");
-//	            userName = System.getenv("DATABASE_USERNAME");
-//	            passWord = System.getenv("DATABASE_PASSWORD");
 
-		String url = "jdbc:mysql://localhost:3306/Collage_management_app";
-		String userName = "root";
-		String passWord = "123456";
+	            url = System.getenv("DATABASE_HOST");
+	            userName = System.getenv("DATABASE_USERNAME");
+	            passWord = System.getenv("DATABASE_PASSWORD");
 
-		try {
+	        try {
 
-			con = DriverManager.getConnection(url, userName, passWord);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("Unable to connect to the database");
-		}
-		return con;
-	}
+	            con = DriverManager.getConnection(url, userName, passWord);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	            throw new RuntimeException("Unable to connect to the database");
+	        }
+	        return con;
+	    }
+}
+	
 }
